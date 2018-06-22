57335f3124025463138b6f5a9172d59ac5c6acfa
Freddy <andrewsoto@hotmail.es>
2018-06-21 22:37:33 -0600

    Migraciones no Patologicos

--- database/migrations/2018_06_22_040035_create_no_patologicos_table.php
+++ database/migrations/2018_06_22_040035_create_no_patologicos_table.php
@@ -16,9 +16,9 @@ class CreateNoPatologicosTable extends Migration
         Schema::create('noPatologicos', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('paciente_id')->unsigned();
-            $table->enum('alcohol',['Si','No'])->default('No');
-            $table->enum('tabaco',['Si','No'])->default('No');
-            $table->enum('medicacion',['Si','No'])->default('No');
+            $table->enum('alcohol',['Mucho','Poco','Nunca'])->default('Nunca');
+            $table->enum('tabaco',['Mucho','Poco','Nunca'])->default('Nunca');
+            $table->enum('medicacion',['Mucho','Poco','Nunca'])->default('Nunca');
 
             $table->foreign('paciente_id')->references('id')->on('pacientes');
             $table->timestamps();
